6df006dc36d6f5b02e08c1ce817567bb3424fb07
kevinjsheen <kevinjsheen@gmail.com>
2016-04-30 19:29:40 -0400

    Completed Assignment Requirements

--- js/main.js
+++ js/main.js
@@ -7,15 +7,70 @@
    to reference the Bootstrap documentation.)
 */
 
+//  L.tileLayer('https://api.tiles.mapbox.com/v4/{z}/{x}/{y}.png', {
+//    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//    maxZoom: 18,
+//    id: 'mapbox://styles/kevinsheen/cinkpcd4u001nakm5ivnjibh6',
+//    accessToken: 'pk.eyJ1Ijoia2V2aW5zaGVlbiIsImEiOiJjaW5rbmkybGwweWtydWdseXkwZGNmOG5sIn0.hMMX1QOPMDgHhTskXJVz6g'
+// }).addTo(map);
+	
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
+
+	$(document).ready(function() {
+	
+	var map = L.map( 'map', {
+    center: [46.8523, -121.7603],
+    minZoom: 5,
+    zoom: 11
+	});
 
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
 // the map tiles layers:
+// var marker = L.marker
+	
+	L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+	}).addTo(map);
+	
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+	
+	var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+	});
+
+	var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+    subdomains: ['otile1','otile2','otile3','otile4']
+	});
+
+	var mapLayers = {
+    "Satellite": satLayer,
+    "Map View": drawLayer,
+    "Open Street Maps": osm
+	};
+	
+	L.control.layers(mapLayers).addTo(map);
+	satLayer.addTo(map);
 
 
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+	
+	var marker = L.marker([46.8523, -121.7603]).addTo(map);
+	marker.bindPopup("<b>You made it to Mount Rainier!</b>");
+  
+	var marker = L.marker([46.9420, -122.6060]).addTo(map);
+	marker.bindPopup("<b>Try out some nature in Yelm!</b>");
+	
+	var marker = L.marker([47.0379, -122.9007]).addTo(map);
+	marker.bindPopup("<b>Washington State Capitol!</b>"); 
+
+	});
